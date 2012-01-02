6cb55f7077d1585c580066c7d40988d83bdd2c31
Taras Nek <mr.taras1k@gmail.com>
2012-01-02 11:15:54 +0200

    tests

--- test_main.py
+++ test_main.py
@@ -1,6 +1,11 @@
 from attest import Tests
 import helper
+import requests
+
 generate = Tests()
+code = Tests()
+load = Tests()
+
 
 @generate.test
 def gen():
@@ -8,7 +13,26 @@ def gen():
 		str = helper._id_generator(i)
 		assert i == len(str)
 
+
+@code.test
+def chek():
+	server_url = 'http://0.0.0.0:8080/'
+	url = helper.short_it('ya.ru')
+	r = requests.get(server_url+url)
+	assert r.status_code == 200		
+
+
+@load.test
+def chek():
+	server_url = 'http://0.0.0.0:8080/'
+	url = helper.short_it('ya.ru')
+	for i in range(1000):
+		r = requests.get(server_url+url)
+		assert r.status_code == 200			
+
 if __name__ == '__main__':
 	generate.run()
+	code.run()
+	load.run()
 
 
